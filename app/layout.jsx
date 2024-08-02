d3fad2ace4b0bb3833338c5c3d1af5ea4d011bfe
ZEZE_COBRA <yeridsonruan995@gmail.com>
2024-08-02 20:35:17 -0300

    The Man Blog

---
+++ app/layout.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import './globals.css';
+
+export const metadata = {
+  title: 'The Man BLOG',
+  description:
+    'Um site de recordação dos membros do grupo do whatsapp The Man Evolution.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='pt-br'>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Card from './components/card';
+
+export default function Home() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Card />
+      </main>
+      <footer></footer>
+    </>
+  );
+}
